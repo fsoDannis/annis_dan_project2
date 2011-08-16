d58a5592f4f8fb777cb2adeeae4345e8c12c9992
Dan <Dan@Dinkydetails.com>
2011-08-16 18:07:24 -0400

    Updated with Corrections

--- main.js
+++ main.js
@@ -1,55 +1,57 @@
+// Define Vars
+var sel				=	document.getElementById('jobtime');
+var curOpt 			= 	sel.selectedIndex;
+var value			= 	sel.options[sel.selectedIndex].text;
+var sup; 				
+
 function getItems(){
 	if(localStorage.getItem('appwhatproject')){
 		var timeValue			=	localStorage.getItem('appjobtime');
 		var whatproject			= 	localStorage.getItem('appwhatproject');
 		var comments			=	localStorage.getItem('appcomments');
-		var range				=	localStorage.getItem('apprange');
-	
+		var sup					= 	localStorage.getItem('appSuperName');
+		var rangeSlider			=	localStorage.getItem('appRange');
+		
 		var viewAll = [
 			timeValue,
 			whatproject,
-			range,
 			comments,
+			sup,
+			rangeSlider
+			
 		];
 			
 		alert(viewAll);
-		//document.getElementById('main').style.display="none";
-		//var clearLink= document.getElementById('clear');
-	//	clearLink.style.display ="block";
-		}
+	}
 }
 
 
 function storeItems(id){
+	getRadioButtonValue();
 	var whatproject		= 	document.getElementById('whatproject').value;
 	var comments		=	document.getElementById('comments').value;
-	// getting my selector Items
 	var selTime			=	document.getElementById('jobtime');
 	var timeValue		= 	selTime.options[sel.selectedIndex].text;
-	var range			=	document.getElementById('range').value;
+	var rangeSlider      = 	document.getElementById('slider').value;
 	
 	localStorage.setItem('appwhatproject',whatproject);
 	localStorage.setItem('appcomments', comments);
 	localStorage.setItem('appjobtime', timeValue);
-	localStorage.setItem('apprange', range);
+	localStorage.setItem('appSuperName', sup);
+	localStorage.setItem('appRange', rangeSlider);
+	
+	
 }
 
-//function clearLocal(){
-	localStorage.clear();
-	return false;
-}	
 
-var sup;  // just couldn't get the radials to work! 
 
 function getRadioButtonValue(){
 	var radioLength = document.forms[0].sup.length;
-//Find out how many radio buttons there are with the name of sup.
 	for (i=0; i < radioLength; i++){
-//Loop through each radio button and find out which one is checked.
-	if(document.forms[0].sup[i].checked == true){
-//When we find the radio button that's checked, set the sup var to the value of the radio button.
-		sup = document.forms[0].sup[i].value;
+		if(document.forms[0].sup[i].checked == true){
+			sup = document.forms[0].sup[i].value;
 		}
 	}
-} //Radials no worky.
+}
+
 
